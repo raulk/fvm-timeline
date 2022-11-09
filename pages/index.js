42bf9ac4fa55a38aa452ddb9530236e29401139c
Maciej Witowski <witowski.maciej@gmail.com>
2022-11-09 17:11:34 +0100

    Update dates (#2)
    Co-authored-by: raulk <raul.kripalani@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,10 @@ const Countdown = dynamic(() => import('react-countdown'), {
 })
 
 const deadlines = [
-  { milestone: "End of development", date: "2022-11-08", colors: "bg-slate-800 text-slate-100 hover:text-amber-100 hover:bg-amber-600", c: "bg-amber-600/50" },
+  { milestone: "End of development", date: "2022-12-06", colors: "bg-slate-800 text-slate-100 hover:text-amber-100 hover:bg-amber-600", c: "bg-amber-600/50" },
   { milestone: "Lisbon events kickoff", date: "2022-10-24", colors: "bg-slate-800 text-slate-100 hover:text-pink-100 hover:bg-pink-600", c: "bg-pink-600/50" },
-  { milestone: "Audits kickoff", date: "2022-11-14", colors: "bg-slate-800 text-slate-100 hover:text-red-100 hover:bg-red-600", c: "bg-red-600/50" },
-  { milestone: "Butterfly launch", date: "2022-11-14", colors: "bg-slate-800 text-slate-100 hover:text-violet-100 hover:bg-violet-600", c: "bg-violet-600/50" },
-  { milestone: "Buildernet launch", date: "2022-11-28", colors: "bg-slate-800 text-slate-100 hover:text-sky-100 hover:bg-sky-600", c: "bg-sky-600/50" },
+  { milestone: "Audits kickoff", date: "2022-11-22", colors: "bg-slate-800 text-slate-100 hover:text-red-100 hover:bg-red-600", c: "bg-red-600/50" },
+  { milestone: "Stable testnet + Acceleration", date: "2022-12-06", colors: "bg-slate-800 text-slate-100 hover:text-sky-100 hover:bg-sky-600", c: "bg-sky-600/50" },
   { milestone: "Calibrationnet launch", date: "2023-01-09", colors: "bg-slate-800 text-slate-100 hover:text-lime-100 hover:bg-lime-600", c: "bg-lime-600/50" },
   { milestone: "Mainnet", date: "2023-02-08", colors: "bg-slate-800 text-slate-100 hover:text-green-100 hover:bg-green-600", c: "bg-green-600/50" },
 ];
